eae178d37253af8bb4d265f5f8bf22dc84c50df4
kanrisha-daikou <fanxionggongteng3@gmail.com>
2025-11-28 20:37:31 +0900

    Add root path to redirect to login page

--- acco/urls.py
+++ acco/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import SignUpView, SignSuccessView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import RedirectView
 
 
 app_name = 'acco'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='login/')),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signup_success/', SignSuccessView.as_view(), name='signup_success'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
